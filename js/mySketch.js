baa43dadc76ed5d9cf16e68d6cbfbbb406ce58b2
nbogie <neillbogie@googlemail.com>
2023-11-22 17:43:09 +0000

    initial from my openprocessing sketch
    https://openprocessing.org/sketch/1004604

---
+++ js/mySketch.js
@@ -0,0 +1,30 @@
+// An interactive coordinate system reference diagram and tool
+//for students and teachers.
+//
+// See interaction tab for keyboard commands.
+//
+// There is also a simpler non-interactive version available:
+// https://www.openprocessing.org/sketch/1004603
+
+function setup() {
+    //Fancier way of creating a canvas so that it is a child of a specific element
+    let canvasParent = document.getElementById("sketch-holder");
+    createCanvas(400, 400).parent(canvasParent);
+
+    noCursor();
+    textAlign(CENTER, CENTER);
+    resetAllMarkers();
+}
+
+function draw() {
+    background(255);
+    drawGridLines();
+    drawGridLabels();
+    drawXAxisArrow();
+    drawYAxisArrow();
+    drawXLine();
+    drawYLine();
+
+    drawLabeledMousePosition();
+    drawMarkers();
+}
